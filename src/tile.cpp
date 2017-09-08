557848283e3f0de9c99617b65ce9cefe4c9ef0c8
Vova Myroniuk <vladimironiuk@gmail.com>
2017-09-08 11:41:18 +0300

    Prepare tile to collision detection

--- src/tile.cpp
+++ src/tile.cpp
@@ -4,3 +4,49 @@ Tile::Tile(QObject *parent) : BaseItem(parent)
 {
 
 }
+
+bool Tile::isTankTraversable() const
+{
+    // TODO: handle ice for tank
+    return material_ & (Free | Bush);
+}
+
+bool Tile::isBombTraversable() const
+{
+    return material_ & (Free | Bush | Ice | Water);
+}
+
+bool Tile::isBombBreakable() const
+{
+    // TODO: handle concrete for three stars tank's bomb
+    return material_ & Bricks;
+}
+
+void Tile::setMaterial(Material material)
+{
+    if (material_ == material)
+        return;
+    material_ = material;
+    updateImageSource(material_);
+    emit materialChanged(material_);
+}
+
+void Tile::updateImageSource(Material material)
+{
+    switch (material) {
+    case Bricks:
+        setImageSource("qrc:/images/obstacles/bricks.png");
+        break;
+    case Concrete:
+        setImageSource("qrc:/images/obstacles/concrete.png");
+        break;
+        // TODO: add image source below
+    case Bush:
+    case Ice:
+    case Water:
+        // fall down
+    case Free:
+    default:
+        setImageSource("");
+    }
+}
--- src/tile.h
+++ src/tile.h
@@ -7,7 +7,31 @@ class Tile : public BaseItem
 {
     Q_OBJECT
 public:
+    enum Material {
+        Free,
+        Bricks,
+        Concrete,
+        Bush,
+        Ice,
+        Water
+    };
+
     explicit Tile(QObject *parent = nullptr);
+
+    bool isTankTraversable() const;
+    bool isBombTraversable() const;
+    bool isBombBreakable() const;
+
+    Material material() const { return material_; }
+    void setMaterial(Material material);
+
+signals:
+    void materialChanged(Material material);
+
+private:
+    void updateImageSource(Material material);
+
+    Material material_ = Free;
 };
 
 #endif // TILE_H
